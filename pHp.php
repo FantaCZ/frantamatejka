2f074b15e216df9efeae53b4fc72550157032e2b
user <user@matejka>
2020-04-17 11:44:50 +0200

    Merge origin/master
    Conflicts:
    	florbal.html

---
+++ pHp.php
@@ -0,0 +1,30 @@
+<?php
+$data = "pansízn";
+
+
+$a = 4;
+$b = 2;
+
+
+
+$pravda = true;        //boolean 
+$jméno = "Lindemann";  //string
+$desetine = 10.5;      //float
+$cislo = 2;            //string
+
+
+
+
+echo "legolas";
+echo '<br>';
+echo $data;
+echo '<br>';
+echo "<strong>sabaton</strong>";
+
+var_dump($a+$b);
+var_dump($a == $b);
+
+
+
+
+?>
